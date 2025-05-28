808a719cfae88f9e27163586bf44229c74da8a88
RAVEN850972 <timmendax@gmail.com>
2025-05-28 21:45:21 +0500

    Delete neurograph directory

--- neurograph/contextvec/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Модуль для работы с векторными представлениями (эмбеддингами)."""
-
-from neurograph.contextvec.base import IContextVectors
-from neurograph.contextvec.factory import ContextVectorsFactory
-
-__all__ = [
-    "IContextVectors",
-    "ContextVectorsFactory"
-]
--- neurograph/memory/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Модуль для работы с многоуровневой биоморфной памятью."""
-
-from neurograph.memory.base import IMemory, MemoryItem
-
-__all__ = [
-    "IMemory",
-    "MemoryItem"
-]
